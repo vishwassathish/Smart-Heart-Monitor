82cefacb2d4677c8135b3d3d7b078afe26f7cb87
Quantum97 <vishwassathish@gmail.com>
2017-05-03 20:50:01 +0530

    Add files via upload

---
+++ PulseSensorAmped_Arduino_1.5.0/PulseSensorAmped_Arduino_1.5.0.ino
@@ -0,0 +1,84 @@
+/*  
+
+----------------------  Notes ----------------------  ----------------------
+This code:
+1) Blinks an LED to User's Live Heartbeat   PIN 13
+2) Fades an LED to User's Live HeartBeat    PIN 5
+3) Determines BPM
+4) Prints All of the Above to Serial
+
+
+ ----------------------       ----------------------  ----------------------
+*/
+#include <LiquidCrystal.h>
+#include <stdlib.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+#define PROCESSING_VISUALIZER 1
+#define SERIAL_PLOTTER  2
+
+//  Variables
+int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 13;                // pin to blink led at each beat
+int fadePin = 7;                  // pin to do fancy classy fading blink at each beat
+int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
+
+// Volatile Variables, used in the interrupt service routine!
+volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
+volatile int Signal;                // holds the incoming raw data
+volatile int IBI = 600;             // int that holds the time interval between beats! Must be seeded!
+volatile boolean Pulse = false;     // "True" when User's live heartbeat is detected. "False" when not a "live beat".
+volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
+
+// SET THE SERIAL OUTPUT TYPE TO YOUR NEEDS
+
+// SERIAL_PLOTTER outputs sensor data for viewing with the Arduino Serial Plotter
+
+static int outputType = SERIAL_PLOTTER;
+
+
+void setup(){
+  lcd.begin(16, 2);
+  lcd.print("Welcome to Heart-Beat Detector");
+  delay(100);
+  lcd.setCursor(0,1);
+  lcd.print("Sensing your BPM...");
+  pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
+  pinMode(fadePin,OUTPUT);          // pin that will fade to your heartbeat!
+  Serial.begin(115200);             // we agree to talk fast!
+  interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS
+   
+}
+
+
+//  Where the Magic Happens
+void loop(){
+      lcd.clear();
+  start: //label 
+ 
+  lcd.setCursor(0, 0);
+  lcd.print("BPM = ");
+  lcd.print(BPM);
+  //delay (100);
+  lcd.setCursor(0, 1); // set the cursor to column 0, line 2
+  
+    serialOutput() ;
+
+  if (QS == true){     // A Heartbeat Was Found
+                       // BPM and IBI have been Determined
+                       // Quantified Self "QS" true when arduino finds a heartbeat
+        fadeRate = 255;         // Makes the LED Fade Effect Happen
+                                // Set 'fadeRate' Variable to 255 to fade LED with pulse
+        serialOutputWhenBeatHappens();   // A Beat Happened, Output that to serial.
+        QS = false;                      // reset the Quantified Self flag for next time
+  }
+
+  ledFadeToBeat();                      // Makes the LED Fade Effect Happen
+  delay(20);                             //  take a break
+}
+
+
+void ledFadeToBeat(){
+    fadeRate -= 15;                         //  set LED fade value
+    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
+    analogWrite(fadePin,fadeRate);          //  fade LED
+  }
